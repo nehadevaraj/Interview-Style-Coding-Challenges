6fc163833ee41fdfe06da488cf46c433d9b9f004
nehadevaraj <146327712+nehadevaraj@users.noreply.github.com>
2023-11-30 14:48:01 +0530

    InverseMatrix.py

--- InverseMatrix.py
+++ InverseMatrix.py
@@ -35,7 +35,8 @@ def inverse_2x2(matrix):
 This line defines a function named inverse_2x2 that takes a 2x2 matrix as input.
 
 a, b = matrix[0, 0], matrix[0, 1]:
-These lines extract the elements of the input matrix using array indexing. a and b represent the elements in the first row of the first column and first row of the second column, respectively.
+These lines extract the elements of the input matrix using array indexing. a and b represent the elements in the first row of the first column and 
+first row of the second column, respectively.
 
 c, d = matrix[1, 0], matrix[1, 1]:
 Similarly, these lines extract the elements in the second row of the first column and second row of the second column, assigning them to variables c and d.
@@ -67,5 +68,8 @@ This line prints a header indicating that the following output is the inverse of
 print(inverse_A):
 This line prints the calculated inverse matrix to the console.
 
-In summary, the script defines a function to calculate the inverse of a 2x2 matrix and demonstrates its usage with an example matrix. The inverse matrix is printed to the console if it exists; otherwise, a ValueError is raised. Calculating the inverse of a matrix is a fundamental operation in linear algebra and is often used in various fields, including computer graphics, optimization, and statistics.
+In summary, the script defines a function to calculate the inverse of a 2x2 matrix and demonstrates its usage with an example matrix. 
+The inverse matrix is printed to the console if it exists; otherwise, a ValueError is raised. 
+Calculating the inverse of a matrix is a fundamental operation in linear algebra and is often used in various fields, including computer graphics, optimization, 
+and statistics.
 '''
